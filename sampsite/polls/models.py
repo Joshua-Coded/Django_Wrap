ee1b3c3782e5c023ec7e358f0caf1992f9bb5261
joshbam-joshwilly <j.alana@alustudent.com>
2024-02-01 17:45:27 +0100

    done for now

--- sampsite/polls/models.py
+++ sampsite/polls/models.py
@@ -7,6 +7,12 @@ class Question(models.Model):
 
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 
 class Choice(models.Model):
@@ -15,3 +21,7 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
 
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+    
